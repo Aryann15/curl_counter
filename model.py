ade3e0b1ccfa486ec4b732c8eca858f47d6298aa
Aryann15 <rajlysm15@gmail.com>
2023-06-30 11:42:59 +0530

    predict metjod

--- model.py
+++ model.py
@@ -28,4 +28,15 @@ class Model:
         self.model.fit(img_list, class_list)
         print("Model successfully trained!")
 
+    def predict(self, frame):
+        frame = frame[1]
+        cv2.imwrite("frame.jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY))
+        img = PIL.Image.open("frame.jpg")
+        img.thumbnail((150,150), PIL.Image.ANTIALIAS)
+        img.save("frame.jpg")
+
+        img= cv2.imread("frame.jpg")[:,:,0]
+        img= img.reshape(16950)
+        prediction = self.model.predict([img])
+        return prediction[0]
 
