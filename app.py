adbd57c8ef3e396346ffb25e78414995867f8388
Aryann15 <rajlysm15@gmail.com>
2023-06-30 10:33:56 +0530

    classify images o fextended and contracted arms

---
+++ app.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import os
+import PIL.Image, PIL.ImageTk
+import cv2
+import camera
+
+
+class App:
+
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title = "Curl Counter"
+
+        self.counters = [1,1]
+        self.rep_counter = 0
+
+        self.extended= False
+        self.contracted= False
+
+
