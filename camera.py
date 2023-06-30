51b2bc78ff18538c64eed417d64ef5588a9b2362
Aryann15 <rajlysm15@gmail.com>
2023-06-30 10:13:53 +0530

    initializes a camera object by opening the default camera device and checks whether it was opened successfully

--- camera.py
+++ camera.py
@@ -3,3 +3,6 @@ import cv2
 class Camera:
     def __init__(self):
         self.camera  = cv2.VideoCapture(0)
+        if not self.camera.isOpened():
+            raise ValueError("Camera not Found!")
+
