fa4f73909ccbdc09ab749196efda4793c3d5409a
Aryann15 <rajlysm15@gmail.com>
2023-07-28 23:33:16 +0530

    using my android camera

--- camera.py
+++ camera.py
@@ -1,13 +1,17 @@
-import cv2
-
+import cv2 as cv
+import requests
+import imutils
+import numpy as np
+url = "http://192.168.1.2:8080/shot.jpg"
 class Camera:
     def __init__(self):
-        self.camera  = cv2.VideoCapture(0)
-        if not self.camera.isOpened():
-            raise ValueError("Camera not Found!")
+        img_resp = requests.get(url)
+        img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+        img = cv.imdecode(img_arr, -1)
+        img = imutils.resize(img, width=900, height=900)
+        self.camera  = img
 
-        self.width =self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height =self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
 
     def __del__(self):
         if self.camera.isOpened():
@@ -18,9 +22,16 @@ class Camera:
             ret, frame = self.camera.read()
 
             if ret:
-                return ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                return ret, cv.cvtColor(frame, cv.COLOR_BGR2RGB)
             else:
                 return ret,None
         else:
             return None
+
+camera = Camera()
+ret,frame = camera.get_frame()
+if ret:
+    cv.imshow("camera feed", frame)
+    cv.waitKey(0)
+    cv
 
